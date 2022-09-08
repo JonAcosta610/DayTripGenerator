3a32e0c081a929a879f0979e8db4f3adccc6a07a
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-09-07 21:42:58 -0400

    Added lists for destinations, restaurants, mode of transportation and entertainment.

---
+++ main.py
@@ -0,0 +1,5 @@
+
+destinations = ["Jamaica", "Hawaii", "Aruba", "Mal Dives", "Dominican Republic", "Puerto Rico"]
+reataurants = ["Cara Mia", "Applebees", "Reethi Restaurant", "Symphony Restaurant", "Sea House Cafe", "Murphys West End Restaurant"]
+mode_of_transportation = ["plane", "cruise ship", "train", "car", "bus", "swimming", "mopes", "bicycles"]
+entertainment_list = ["bus tour", "scuba diving", "dolphin watching", "surfing", "plantation tours", "historic site tours"]
