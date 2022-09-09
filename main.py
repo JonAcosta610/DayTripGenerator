9377a8e1689c9dce598b9a58dfebd555769113f9
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-09-08 22:41:51 -0400

    Updated the randomizer function to include randomizer choices because I noticed while debugging when users tried to change a selection it wouldn't re-randomize. I also added a print statement owards the bottom that wraps up what the user's day will look like.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 destinations = ["Jamaica", "Hawaii", "Aruba", "Mal Dives", "Dominican Republic", "Puerto Rico"]
 restaurants = ["Cara Mia", "Applebees", "Reethi Restaurant", "Symphony Restaurant", "Sea House Cafe", "Murphys West End Restaurant"]
 mode_of_transportation = ["plane", "cruise ship", "train", "car", "bus", "motorcycle", "mopeds", "bicycles"]
-entertainment_list = ["bus tour", "scuba diving", "dolphin watching", "surfing", "plantation tours", "historic site tours"]
+entertainment_list = ["bus tour", "scuba diving lesson", "dolphin watching tour", "surfing lesson", "plantation tour", "historic site tour"]
 
 import random
 
-random_destination = random.choice(destinations)
-random_restaurant = random.choice(restaurants)
-random_transportation = random.choice(mode_of_transportation)
-random_entertainment = random.choice(entertainment_list)
 
 print("Welcome to Trip Planner! Where we will plan your trip.")
 
 def day_trip_generator():
+    random_destination = random.choice(destinations)
+    random_restaurant = random.choice(restaurants)
+    random_transportation = random.choice(mode_of_transportation)
+    random_entertainment = random.choice(entertainment_list)
+    
     print(f"Destination: {random_destination}")
     print(f"Restaurant: {random_restaurant}")
     print(f"Mode of Transportation: {random_transportation}")
@@ -38,6 +39,11 @@ def itinerary():
         elif user_replace == "Entertainment":
             random_entertainment = random.choice(entertainment_list)
             day_trip_generator()
+        
+        user_satisfaction = input("Are you satisfied with this trip? Yes or No ")
+
+    else:
+        print("Fantastic! Have a safe trip!")
     
-    user_satisfaction = input("Are you satisfied with this trip? Yes or No ")
 itinerary()
+print(f"Here is your full day trip itinerary. You will be traveling to {random_destination}. We have made reservations for your meal at {random_restaurant}. While on the island you will be using {random_transportation} as your primary mode of transportation. And to cap the day you will take {random_entertainment}.")
